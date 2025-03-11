f509a3b7d25ff4cc3739fdd50379f14b8be6c873
Fortitude-lan <358098351@qq.com>
2025-03-11 15:20:32 +0800

    qr generate

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className="relative min-h-[100vh] h-full flex justify-center items-center">
       <QrCodeGenerator />
-      <Image
+      {/* <Image
         src="/glass.png"
         width={1600}
         height={1200}
         alt="glass image"
         className="fixed top-0 left-0 w-full h-full object-cover z-0 pointer-events-none"
-      />
+      /> */}
     </div>
   );
 }
